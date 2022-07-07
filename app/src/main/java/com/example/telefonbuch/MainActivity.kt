ca4bb78741edeb835b73448217d87e1a57a3d1b5
Kevin Later <laterkevin@icloud.com>
2022-07-07 14:09:36 +0200

    Layout Design

--- app/src/main/java/com/example/telefonbuch/MainActivity.kt
+++ app/src/main/java/com/example/telefonbuch/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.telefonbuch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.telefonbuch.contactadapter.ContactAdapter
 import com.example.telefonbuch.data.Datasource
 import com.example.telefonbuch.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,5 +29,31 @@ class MainActivity : AppCompatActivity() {
         binding.contactRecycler.adapter = ContactAdapter(this, contact)
 
         binding.contactRecycler.setHasFixedSize(true)
+    }
+    class MainActivity : AppCompatActivity() {
+        lateinit var editText: EditText
+        lateinit var textView: TextView
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            editText = findViewById(R.id.editTextName)
+            editText = findViewById(R.id.editTextTelefonummer)
+            textView = findViewById(R.id.textView)
+            title = "KotlinApp"
+        }
+
+        fun setText(view: View) {
+            val newText = editText.text.toString()
+            textView.text = newText
+            closeKeyBoard()
+        }
+
+        private fun closeKeyBoard() {
+            val view = this.currentFocus
+            if (view != null) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+        }
     }
 }
